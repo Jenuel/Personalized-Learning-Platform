11b185c6e8aad7b73a07c3599ab6d60a0f416a51
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-05-29 13:45:25 +0800

    Created an entity and integrated TypeOrm to app.module

--- cards/src/app.module.ts
+++ cards/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CardsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "password",
+    database: "cards_db",
+    autoLoadEntities: true,
+    synchronize: true,
+  }), CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
