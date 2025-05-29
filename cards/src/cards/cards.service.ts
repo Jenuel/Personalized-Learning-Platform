5b8b03cabe87c1cd808336ac61ddf24f70e8c4cf
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-05-29 13:50:23 +0800

    Integrated the Cards entity to the implementation of the service

--- cards/src/cards/cards.service.ts
+++ cards/src/cards/cards.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cards } from './cards.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CardsService {
-    getAllCards() {
-        return "List of all cards";
+    constructor(
+        @InjectRepository(Cards)
+        private readonly cardsRepository: Repository<Cards>
+    ) {}
+    async getAllCards(): Promise<Cards[]> {
+        return this.cardsRepository.find();
     }
 }
