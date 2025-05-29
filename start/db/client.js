371bade73410fce3f43daddf9b2d5297d184fe34
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-05-29 21:52:55 +0800

    Pasted the application committed to the other repo

---
+++ start/db/client.js
@@ -0,0 +1,25 @@
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+    user: process.env.DB_USER || 'your_db_user',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'your_db_name',
+    password: process.env.DB_PASSWORD || 'your_db_password',
+    port: process.env.DB_PORT || 5432,
+});
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log('Connected to PostgreSQL Database');
+        return client;
+    } catch (err) {
+        console.error('PostgreSQL connection error:', err.message);
+        throw err; 
+    }
+}
+
+export default connectDB;
---
+++ start/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the Assessment API");
+});
+
+async function startServer() {
+  try {
+    await connectDB();
+
+    app.listen(3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+
+  } catch (err) {
+    console.error('Failed to start server due to DB error.');
+    process.exit(1);
+  }
+}
+
+startServer();
+
