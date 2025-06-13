0e5298bf7f0f9e89c7ee1b39fe403aa4294ef63e
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-13 16:28:25 +0800

    Add @nestjs/config for environment configuration and refactor TypeOrmModule initialization

--- cards/src/app.module.ts
+++ cards/src/app.module.ts
@@ -1,19 +1,30 @@
-import { Module } from '@nestjs/common';
+import { ConfigurableModuleBuilder, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cards } from './cards/cards.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "password",
-    database: "cards_db",
-    autoLoadEntities: true,
-    synchronize: true,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USERNAME', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', 'password'),
+        database: configService.get<string>('DB_DATABASE', 'cards_db'),
+        entities: [Cards],
+        synchronize: true,
+      }),
   }), CardsModule],
   controllers: [AppController],
   providers: [AppService],
