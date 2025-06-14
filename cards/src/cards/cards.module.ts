7324e25a1df770eeca0caed97c019c6675de2edf
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-14 15:58:50 +0800

    Import TypeOrmModule and register Cards entity in CardsModule

--- cards/src/cards/cards.module.ts
+++ cards/src/cards/cards.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // import this
 import { CardsController } from './cards.controller';
 import { CardsService } from './cards.service';
+import { Cards } from './cards.entity'; // import your entity
+
 
 @Module({
+   imports: [TypeOrmModule.forFeature([Cards])], 
   controllers: [CardsController],
   providers: [CardsService]
 })
