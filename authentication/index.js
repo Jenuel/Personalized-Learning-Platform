13286ea554694d980ebcf1a81b51f2d1c7fbd23a
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-15 13:51:33 +0800

    Created the template of routes and integrated it to index.js

--- authentication/index.js
+++ authentication/index.js
@@ -1,22 +1,24 @@
 import express from "express"
 import { connectDB } from "./db/client"
+import authRoutes from "./routes/authRoutes.js"
 
-dotenv.config()
+dotenv.config();
 
-app = express()
+app = express();
 
+app.use("/auth", authRoutes);
 
 async function startServer() {
     try {
         await connectDB;
 
         app.listen(process.env.PORT, () => {
-            console.log(`Authentication server running on ${process.env.PORT}`)
+            console.log(`Authentication server running on ${process.env.PORT}`);
         })
 
     } catch (error) {
-        console.error("Cannot connect to database")
-        process.exit(1)
+        console.error("Cannot connect to database");
+        process.exit(1);
     }
 }
 
---
+++ authentication/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+router = Router()
+
+router.post("/login")
+
+router.post("/register")
+
+router.put("/update")
+
+router.delete("/delete")
+
+export default router;
