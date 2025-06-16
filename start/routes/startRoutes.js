ab98ec711923bab2c7fc98accf43a238fceccbd6
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-16 21:25:53 +0800

    Refactor update route to use handleCards function for improved card status management

--- start/routes/startRoutes.js
+++ start/routes/startRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getDueCards, getAllCards, updateCardsStatus } from "../controllers/startController.js";
+import { getDueCards, getAllCards, handleCards } from "../controllers/startController.js";
 
 router = express.Router();
 
@@ -7,6 +7,6 @@ router.get("/due", getDueCards);
 
 router.get("/all", getAllCards);
 
-router.patch("/update", updateCardsStatus);
+router.put("/update", handleCards);
 
 export default router;
