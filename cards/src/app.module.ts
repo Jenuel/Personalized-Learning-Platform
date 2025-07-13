b40ae673f3450f2be6e6aebe1acf15681538df85
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-07-13 11:08:00 +0800

    Changed the port and added CardMetadata in entities

--- cards/src/app.module.ts
+++ cards/src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Cards } from './cards/cards.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { Cards } from './cards/cards.entity';
+import { CardMetadata } from './cards/cardmetadata.entity';
 
 @Module({
   imports: [
@@ -18,11 +18,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 5432),
+        port: configService.get<number>('DB_PORT', 5431),
         username: configService.get<string>('DB_USERNAME', 'postgres'),
         password: configService.get<string>('DB_PASSWORD', 'password'),
         database: configService.get<string>('DB_DATABASE', 'cards_db'),
-        entities: [Cards],
+        entities: [Cards, CardMetadata],
         synchronize: true,
       }),
   }), CardsModule],
