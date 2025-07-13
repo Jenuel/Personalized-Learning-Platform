06a9bf3f6eec6d09f0b774224139f71832e133ce
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-07-13 11:49:02 +0800

    Fixed the structure of the cardUpdateSchema

--- start/schema/cardRequestSchema.js
+++ start/schema/cardRequestSchema.js
@@ -2,12 +2,13 @@ import { z } from 'zod';
 
 
 export const cardUpdateSchema = z.object({
-  updates: z.array(
-    z.object({
-      card_id: z.number().int().positive(),
-      is_correct: z.boolean(),
-      interval: z.number().int().positive(),
-      ease_factor: z.number().int().positive()
-    })
-  )
+    updates: z.array(
+        z.object({
+          card_id: z.number().int().positive(),
+          is_correct: z.boolean(),
+          interval: z.number().int().nonnegative(),
+          ease_factor: z.number().positive()        
+        })
+    )
 });
+
